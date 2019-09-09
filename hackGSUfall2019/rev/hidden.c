5af8f6aa9341138b70e3b1bb4006477dd18f5e44
Dong Jun Kim <35159798+Immobility@users.noreply.github.com>
2019-09-09 09:45:51 -0500

    Add files via upload

---
+++ hackGSUfall2019/rev/hidden.c
@@ -0,0 +1,30 @@
+/*
+Author: Immobility
+
+HackGSU Fall 2019
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_banner()
+
+fflush(NULL);
+}
+int main()
+{
+	print_banner();
+	
+	char input[256];
+
+	printf("It's a simple spell, but quite unbreakable. \n");
+	scanf("%s", input);
+	return 0;
+	
+}
+
+int dontLookInsidePls()
+{
+	printf("I hate coding. GSUCTF{plS_n0_p33KiNg_wh3N_1_c0d3}");
+}
