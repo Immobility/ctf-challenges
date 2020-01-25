e5f852962e74e78b492c4b6d24ef368057784db3
root <root@localhost.localdomain>
2020-01-25 17:24:45 -0500

    added xss filter for first 2 challs ^ flags

--- hackGSUfall2019/web/cats_over_the_network/index.php
+++ hackGSUfall2019/web/cats_over_the_network/index.php
@@ -2,7 +2,19 @@
 
 $host = $_GET['host'];
 $port = $_GET['port'];
-$command = 'nc ' . $host . ' ' . $port;
+
+$conn = $host . ' ' . $port;
+
+blacklist = array('<','>','script', '%3C', '%3E');
+
+foreach ($blacklist as $str) {
+    if(strpos($conn, $str) !== false) {
+        echo "XSS IS NOT THE ANSWER :((((";
+	return;
+    }
+}   
+
+$command = 'nc ' . $conn
 exec($command, $storage);
 
 
